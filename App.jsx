2a7359691e3d104dd406726f904060678796b4e6
mnosumon <mdnuruddin378350@gmail.com>
2025-07-11 15:54:23 +0600

    Bottom tab navigation practice done

--- App.jsx
+++ App.jsx
@@ -4,16 +4,54 @@ import { NavigationContainer } from '@react-navigation/native';
 import Home from './src/screens/tabBottom/Home';
 import Earn from './src/screens/tabBottom/Earn';
 import Delta from './src/screens/tabBottom/Delta';
+import Task from './src/screens/tabBottom/Task';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
+const getIconBars = (routeName, focused, color, size) => {
+  let iconName;
+  if (routeName === 'Home') {
+    iconName = focused ? 'home' : 'home-outline';
+  } else if (routeName === 'Earn') {
+    iconName = focused ? 'cash' : 'cash-outline';
+  } else if (routeName === 'Delta') {
+    iconName = focused ? 'pulse' : 'pulse-outline';
+  } else if (routeName === 'Task') {
+    iconName = focused ? 'list' : 'list-outline';
+  }
+  return <Icon name={iconName} size={size} color={color} />;
+};
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) =>
+            getIconBars(route.name, focused, color, size),
+          tabBarActiveTintColor: 'red',
+          tabBarInactiveTintColor: 'blue',
+          tabBarLabelStyle: {
+            fontSize: 16,
+            fontFamily: 'Georgia',
+            fontWeight: 700,
+          },
+        })}
+      >
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Earn" component={Earn} />
         <Tab.Screen name="Delta" component={Delta} />
+        <Tab.Screen
+          name="Task"
+          component={Task}
+          options={{
+            tabBarBadge: 3,
+            tabBarBadgeStyle: {
+              color: 'black',
+              backgroundColor: 'yellow',
+            },
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
