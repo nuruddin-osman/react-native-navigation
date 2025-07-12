8fd498bf9a06fe44db957e1a0d16f947bf83d273
mnosumon <mdnuruddin378350@gmail.com>
2025-07-12 13:14:47 +0600

    Top tab navigation setup done

--- App.jsx
+++ App.jsx
@@ -1,31 +1,53 @@
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import Home from './src/screens/tabBottom/Home';
-import Earn from './src/screens/tabBottom/Earn';
-import Delta from './src/screens/tabBottom/Delta';
-import Task from './src/screens/tabBottom/Task';
-import Icon from 'react-native-vector-icons/Ionicons';
+// import Home from './src/screens/tabBottom/Home';
+// import Earn from './src/screens/tabBottom/Earn';
+// import Delta from './src/screens/tabBottom/Delta';
+// import Task from './src/screens/tabBottom/Task';
+// import Icon from 'react-native-vector-icons/Ionicons';
 
-const Tab = createBottomTabNavigator();
-const getIconBars = (routeName, focused, color, size) => {
-  let iconName;
-  if (routeName === 'Home') {
-    iconName = focused ? 'home' : 'home-outline';
-  } else if (routeName === 'Earn') {
-    iconName = focused ? 'cash' : 'cash-outline';
-  } else if (routeName === 'Delta') {
-    iconName = focused ? 'pulse' : 'pulse-outline';
-  } else if (routeName === 'Task') {
-    iconName = focused ? 'list' : 'list-outline';
-  }
-  return <Icon name={iconName} size={size} color={color} />;
-};
+// const Tab = createBottomTabNavigator();
+// const getIconBars = (routeName, focused, color, size) => {
+//   let iconName;
+//   if (routeName === 'Home') {
+//     iconName = focused ? 'home' : 'home-outline';
+//   } else if (routeName === 'Earn') {
+//     iconName = focused ? 'cash' : 'cash-outline';
+//   } else if (routeName === 'Delta') {
+//     iconName = focused ? 'pulse' : 'pulse-outline';
+//   } else if (routeName === 'Task') {
+//     iconName = focused ? 'list' : 'list-outline';
+//   }
+//   return <Icon name={iconName} size={size} color={color} />;
+// };
+
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import Home from './src/screens/topTab/Home';
+import Calls from './src/screens/topTab/Calls';
+import Status from './src/screens/topTab/Status';
+import { StatusBar, View } from 'react-native';
+
+const Tab = createMaterialTopTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator
+      <View
+        style={{
+          paddingTop: StatusBar.currentHeight,
+          backgroundColor: '#8000ff',
+        }}
+      >
+        <StatusBar
+          animated={true}
+          barStyle="light-content"
+          hidden={false}
+          translucent={true}
+          showHideTransition="fade"
+        />
+      </View>
+      {/* <Tab.Navigator
         screenOptions={({ route }) => ({
           tabBarIcon: ({ focused, color, size }) =>
             getIconBars(route.name, focused, color, size),
@@ -52,6 +74,11 @@ const App = () => {
             },
           }}
         />
+      </Tab.Navigator> */}
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Calls" component={Calls} />
+        <Tab.Screen name="Status" component={Status} />
       </Tab.Navigator>
     </NavigationContainer>
   );
